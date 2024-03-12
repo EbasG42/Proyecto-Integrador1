f8df7b385e8a1824b1ba7ad2a557aec635337f4f
Samub96 <samuel.d.barona@gmai.com>
2024-03-12 16:19:13 -0500

    Creating  Java project

---
+++ work-area/Sistema_de_medicion_de_manos/src/main/java/com/example/sistema_de_medicion_de_manos/SistemaDeMedicionDeManosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_de_medicion_de_manos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMedicionDeManosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeMedicionDeManosApplication.class, args);
+    }
+
+}
---
+++ work-area/Sistema_de_medicion_de_manos/src/test/java/com/example/sistema_de_medicion_de_manos/SistemaDeMedicionDeManosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_de_medicion_de_manos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMedicionDeManosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
