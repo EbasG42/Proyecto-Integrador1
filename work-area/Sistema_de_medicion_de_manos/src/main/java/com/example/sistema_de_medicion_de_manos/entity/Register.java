c7cd9228fc508be8aa40445073a566bd874241e8
Samub96 <samuel.d.barona@gmai.com>
2024-03-12 18:10:58 -0500

    clases para contener los datos

--- work-area/Sistema_de_medicion_de_manos/src/main/java/com/example/sistema_de_medicion_de_manos/entity/Register.java
+++ work-area/Sistema_de_medicion_de_manos/src/main/java/com/example/sistema_de_medicion_de_manos/entity/Register.java
@@ -1,4 +1,15 @@
 package com.example.sistema_de_medicion_de_manos.entity;
 
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
 public class Register {
+
+
+    @OneToMany(mappedBy = "paciente")
+    private List<Pacient> pacients;
+
+
 }
+
